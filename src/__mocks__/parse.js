552dd02935a3741e905ee302bfbeec678bf27bec
Mark Pavel <markpavel02@gmail.com>
2022-06-02 21:58:18 -0400

    Added parse() Mock

---
+++ src/__mocks__/parse.js
@@ -0,0 +1,13 @@
+const Manager = require('../libs/Manager');
+const Engineer = require('../libs/Engineer');
+const Intern = require('../libs/Intern');
+
+jest.mock('../libs/Manager');
+jest.mock('../libs/Engineer');
+jest.mock('../libs/Intern');
+
+module.exports = [
+    new Manager('Smith John', 4, 'smithjohn@email.com', 20),
+    new Engineer('John Doe', 5, 'johndoe@email.com', 'hubgit'),
+    new Intern('Doe John', 6, 'doejohn@email.com', 'loohcs')
+]
